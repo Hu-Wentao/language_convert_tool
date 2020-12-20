ab5744e83ba62e923b93afaf58b7d3336e0020c6
huwentao <hu.wentao@outlook.com>
2020-12-20 19:00:53 +0800

    feat: 实现新IAccess 基本功能

--- src/base.py
+++ src/base.py
@@ -1,5 +1,9 @@
 import datetime
 import abc
+from typing import Optional
+from typing import List
+from sys import stdin
+
 
 class IConvertor(metaclass=abc.ABCMeta):
     def __init__(self, input_full_nm: str, output_postfix: str, annotation_sign: str, output_nm: str = "otp",):
@@ -53,10 +57,12 @@ class IParser(IConvertor):
             annotation_sign="",
             output_nm=output_nm)
 
+
 class IGen(IConvertor):
     """
     将json转换为其他语言
     """
+
     def __init__(self, input_nm: str, output_postfix: str, annotation_sign: str, output_nm: str = "gen"):
         super(IGen, self).__init__(
             input_full_nm=input_nm,
@@ -65,3 +71,111 @@ class IGen(IConvertor):
             output_nm=output_nm
         )
 
+# -----------------------------------------------------------------------------
+
+
+class IAccess:
+    """
+    读写通道, 可以是文件, 控制台 或者其他 IAccess实现类进行读写
+    """
+
+    def __init__(self,value=None):
+        self.value = value
+        pass
+
+    @abc.abstractmethod
+    def read(self) -> object:
+        pass
+
+    @abc.abstractmethod
+    def set(self, value)->object:
+        self.value = value
+        return self.value
+
+    @abc.abstractmethod
+    def write(self) -> Optional[str]:
+        """
+        返回值表示 出错时的提示信息
+        """
+        pass
+
+
+class IProcessor(object):
+    @abc.abstractmethod
+    def run(self, access: IAccess) -> IAccess:
+        """
+        返回值为 access: IAccess, 正常情况下直接运行 access 即可, 
+        但如果有多个Processor, 则需要将 access 作为下一个processor的input
+        """
+        pass
+
+
+class IBaseConverter:
+    """
+    全新的转换基类
+    """
+
+    def __init__(
+        self,
+        access: IAccess,
+        process_list: List[IProcessor],
+    ):
+        self.access = access
+        self.process_list = process_list
+        pass
+
+    @abc.abstractmethod
+    def on_init(self):
+        pass
+# -----------------------------------------------------------------------------
+
+
+class CmdAccess(IAccess):
+    def __init__(self, value: str = '', need_read: bool = True):
+        self.value = value
+        self.need_read = need_read
+
+    def read(self, tips: str = ''):
+        if self.need_read is True:
+            self.value = input(tips)
+        return self.value
+
+    def write(self, leading: str = '', trailing: str = ''):
+        print(leading+self.value+trailing)
+
+
+class CustomProcessor(IProcessor):
+    def __init__(self, on_run):
+        self.on_run = on_run
+
+    def run(self, input: IAccess):
+        v = self.on_run(input.read())
+        input.set(v)
+        return input
+
+
+class CustomConverter(IBaseConverter):
+    def __init__(
+        self,
+        access: IAccess,
+        process_list: List[IProcessor],
+    ):
+        super(CustomConverter, self).__init__(
+            access, process_list=process_list)
+        self.on_init()
+
+    def on_init(self):
+        acc = self.access
+        for proc in self.process_list:
+            acc = proc.run(acc)
+        acc.write()
+
+
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    CustomConverter(
+        access=CmdAccess('请输入:', need_read=False),
+        process_list=[
+            CustomProcessor(on_run=lambda x: x+'asdff')
+        ],
+    )
