f64c24be8153b6c4e81d22266e019c9507786512
huwentao <hu.wentao@outlook.com>
2020-12-27 12:50:25 +0800

    feat(utils.py): 新增low2upper_camel

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import re
 
-def underscore2upper_camel(nm: str)-> str:
+
+def underscore2upper_camel(nm: str) -> str:
     """
     小写下划线 转 大驼峰
     """
@@ -13,11 +14,12 @@ def underscore2upper_camel(nm: str)-> str:
         else:
             res = res + i[0].upper() + i[1:]
         j += 1
-    
-    res = res[0].upper()+res[1:]
+
+    res = res[0].upper() + res[1:]
     if nm.startswith('_'):
-        res = "_"+res
+        res = "_" + res
     return res
+
 
 def upper_camel2underscore(text):
     """
@@ -30,6 +32,16 @@ def upper_camel2underscore(text):
         lst.append(char)
 
     return "".join(lst).lower()
+
+
+def low2upper_camel(text: str) -> str:
+    """
+    小驼峰转大驼峰
+    :param text:
+    :return:
+    """
+    return text[:1].upper()+text[1:]
+
 
 if __name__ == "__main__":
     # print(underscore2upper_camel('wms_asdf'))
